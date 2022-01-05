8770cc729718b31692c0e54475dad7f906177268
ricardo_dev <ricardo.faiaRF@gmail.com>
2022-01-05 12:49:15 +0000

    Exercicio Desativar Bomba

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿namespace DesativarBombaNS {
+    class DesativarBomba
+    {
+        public static Boolean continuarEscolha = true; //Variavel para se saber se o utilizador pode continuar a escolher fios
+        
+        public static String verificacaoBranco(String corEscolhida) {
+            if (corEscolhida.Equals("branco") || corEscolhida.Equals("preto")) {
+                continuarEscolha = false;
+                return "A bomba explodiu!";
+            } else {
+                return "A bomba continua activa! Corta outro fio.";
+            }
+        }
+
+        public static String verificacaoVermelho(String corEscolhida) {
+            if (corEscolhida.Equals("verde")) {
+                continuarEscolha = false;
+                return "Muito bem! Bomba desactivada. Ufa...";
+            } else {
+                continuarEscolha = false;
+                return "A bomba explodiu!";
+            }
+        }
+
+        public static String verificacaoPreto(String corEscolhida) {
+            if (corEscolhida.Equals("branco") || corEscolhida.Equals("verde") || corEscolhida.Equals("laranja")) {
+                continuarEscolha = false;
+                return "A bomba explodiu!";
+            } else {
+                return "A bomba continua activa! Corta outro fio.";
+            }
+        }
+
+        public static String verificacaoLaranja(String corEscolhida) {
+            if (corEscolhida.Equals("preto") || corEscolhida.Equals("vermelho")) {
+                continuarEscolha = false;
+                return "Muito bem! Bomba desactivada. Ufa...";
+            } else {
+                continuarEscolha = false;
+                return "A bomba explodiu!";
+            }
+        }
+
+        public static String verificacaoVerde(String corEscolhida) {
+            if (corEscolhida.Equals("laranja") || corEscolhida.Equals("branco")) {
+                continuarEscolha = false;
+                return "Muito bem! Bomba desactivada. Ufa...";
+            } else {
+                continuarEscolha = false;
+                return "A bomba explodiu!";
+            }
+        }
+
+        public static String verificacaoRoxo(String corEscolhida) {
+            if (corEscolhida.Equals("preto")) {
+                return "A bomba continua activa! Corta outro fio.";
+            } else {
+                continuarEscolha = false;
+                return "A bomba explodiu!";
+            }
+        }
+
+        public static Boolean verificacaoCorValida(String cor) {
+            if (!cor.Equals("branco") && !cor.Equals("vermelho") && !cor.Equals("preto") &&
+                !cor.Equals("laranja") && !cor.Equals("verde") && !cor.Equals("roxo")) {
+                Console.WriteLine("Cor escolhida não existe, tenta novamente!");
+                return false;
+            } else {
+                return true;
+            }
+        }
+        
+        public static void Main() {
+            String corAnterior = ""; //Para verificação do corte que o utilizador pertende fazer
+
+            do {
+                Console.WriteLine("Escolha a cor do fio a cortar:");
+                String corEscolhida = Console.ReadLine().ToLower(); //Escolha da cor que o utilizador pretende cortar
+
+                if (verificacaoCorValida(corEscolhida)) {
+                    switch (corAnterior) {
+                        case "branco":
+                            Console.WriteLine(verificacaoBranco(corEscolhida));
+                            break;
+                        case "vermelho":
+                            Console.WriteLine(verificacaoVermelho(corEscolhida));
+                            break;
+                        case "preto":
+                            Console.WriteLine(verificacaoPreto(corEscolhida));
+                            break;
+                        case "laranja":
+                            Console.WriteLine(verificacaoLaranja(corEscolhida));
+                            break;
+                        case "verde":
+                            Console.WriteLine(verificacaoVerde(corEscolhida));
+                            break;
+                        case "roxo":
+                            Console.WriteLine(verificacaoRoxo(corEscolhida));
+                            break;
+                        default:
+                            Console.WriteLine("A bomba continua activa! Corta outro fio.");
+                            break;
+                    }
+
+                    corAnterior = corEscolhida;
+                }
+                
+            } while (continuarEscolha);
+        }
+    }
+}
---
+++ TestesBomba.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using DesativarBombaNS;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void PassingTestBranco()
+        {
+            Assert.AreEqual("A bomba continua activa! Corta outro fio.", 
+                DesativarBomba.verificacaoBranco("verde"));
+        }
+
+        [Test]
+        public void FailingTestBranco()
+        {
+            Assert.AreEqual("A bomba explodiu!", 
+                DesativarBomba.verificacaoBranco("preto"));
+        }
+        
+        [Test]
+        public void PassingTestVermelho()
+        {
+            Assert.AreEqual("Muito bem! Bomba desactivada. Ufa...", 
+                DesativarBomba.verificacaoVermelho("verde"));
+        }
+
+        [Test]
+        public void FailingTestVermelho()
+        {
+            Assert.AreEqual("A bomba explodiu!", 
+                DesativarBomba.verificacaoVermelho("branco"));
+        }
+        
+        [Test]
+        public void PassingTestPreto()
+        {
+            Assert.AreEqual("A bomba continua activa! Corta outro fio.", 
+                DesativarBomba.verificacaoPreto("roxo"));
+        }
+
+        [Test]
+        public void FailingTestPreto()
+        {
+            Assert.AreEqual("A bomba explodiu!", 
+                DesativarBomba.verificacaoPreto("laranja"));
+        }
+        
+        [Test]
+        public void PassingTestLaranja()
+        {
+            Assert.AreEqual("Muito bem! Bomba desactivada. Ufa...", 
+                DesativarBomba.verificacaoLaranja("preto"));
+        }
+
+        [Test]
+        public void FailingTestLaranja()
+        {
+            Assert.AreEqual("A bomba explodiu!", 
+                DesativarBomba.verificacaoLaranja("branco"));
+        }
+        
+        [Test]
+        public void PassingTestVerde()
+        {
+            Assert.AreEqual("Muito bem! Bomba desactivada. Ufa...", 
+                DesativarBomba.verificacaoVerde("branco"));
+        }
+
+        [Test]
+        public void FailingTestVerde()
+        {
+            Assert.AreEqual("A bomba explodiu!", 
+                DesativarBomba.verificacaoVerde("vermelho"));
+        }
+
+        [Test]
+        public void PassingTestRoxo()
+        {
+            Assert.AreEqual("A bomba continua activa! Corta outro fio.", 
+                DesativarBomba.verificacaoRoxo("preto"));
+        }
+
+        [Test]
+        public void FailingTestRoxo()
+        {
+            Assert.AreEqual("A bomba explodiu!", 
+                DesativarBomba.verificacaoRoxo("roxo"));
+        }
+        
+        
+        [Test]
+        public void CorEscolhidaInvalida()
+        {
+            Assert.AreEqual(false, 
+                DesativarBomba.verificacaoCorValida("rosa"));
+        }
+        
+        [Test]
+        public void CorEscolhidaValida()
+        {
+            Assert.AreEqual(true, 
+                DesativarBomba.verificacaoCorValida("verde"));
+        }
+    }
+}
